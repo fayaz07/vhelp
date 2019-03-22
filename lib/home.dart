1381df8d2b3bd4e2193d11866bd1a561d58b1960
fayaz07 <fayazfz07@gmail.com>
2019-03-22 20:38:52 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:vhelp/home.dart';
+
+void main(){
+  runApp(
+    CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: VHelpSplash(),
+    )
+  );
+}
+
+// ignore: must_be_immutable
+class VHelpSplash extends StatelessWidget{
+  BuildContext _context;
+
+  checkExistence(){
+    Future.delayed(Duration(milliseconds: 2000)).then((v){
+       Navigator.of(_context).pushReplacement(CupertinoPageRoute(
+         builder: (BuildContext context) => Home()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _context = context;
+    checkExistence();
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 250.0,
+          child: Image.asset('assets/vishnu.png'),
+        )
+      )
+    );
+  }
+}
